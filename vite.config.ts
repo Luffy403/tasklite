b00a7b3e61c4b8d8906170e4694925ce8a508580
joker <gdhjyt68vhbe85.h@gmail.com>
2025-11-24 16:53:39 +0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = 'taskLite'
+const repoName = 'tasklite'
 
 export default defineConfig({
 plugins: [react()],
